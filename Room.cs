623ae11019544848fd672ad279eb0681b56bac7f
notxcharles <35784256+notxcharles@users.noreply.github.com>
2025-02-17 16:55:41 +0000

    Room Dimensions

--- Room.cs
+++ Room.cs
@@ -3,15 +3,21 @@
     public class Room
     {
         private string description;
-
-        public Room(string description)
+        private int[] dimensions = new int[2];
+        public Room(string description, int[] dimensions)
         {
             this.description = description;
+            this.dimensions = dimensions;
         }
 
         public string GetDescription()
         {
             return description;
+        }
+
+        public int[] GetDimensions()
+        {
+            return dimensions;
         }
     }
 }
