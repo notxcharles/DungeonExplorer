98f05248de7fca5ec8bb2fdc5b013ab235152c91
Charles <35784256+notxcharles@users.noreply.github.com>
2025-02-19 21:16:07 +0000

    Basic Game Plan

--- Game.cs
+++ Game.cs
@@ -11,7 +11,7 @@ namespace DungeonExplorer
         public Game()
         {
             // Initialize the game with one room and one player
-
+            // could have a difficulty rating which increases the amount of monsters that the player must fight
         }
         public void Start()
         {
@@ -19,13 +19,15 @@ namespace DungeonExplorer
             bool playing = false;
             int[] roomSize = new int[2] { 7, 9 }; //This should be viewed as room area, with +2 to each axis
             int doorPosition = 5; // The door is always placed at the top of the room. Door Position controls the horizontal position
-            int[] monsterCoord = new int[2] { -1, -1 };
-            int[] treasureCoord = new int[2] { -1, -1 };
+            int[] monsterCoord = new int[2] { 3, 5 }; // Setting to -1,-1 indicates no monster
+            int[] treasureCoord = new int[2] { 3, 2 }; // Setting to -1,-1 indicates no treasure
             Room r = new Room("first", roomSize, doorPosition, monsterCoord, treasureCoord);
             while (playing)
             {
-                // Code your playing logic here
-                
+                // Let's create a game where the player needs to fight monsters in multiple rooms before completing the game to find a treasure in the final room
+                // Each room has one monster which the player much fight before the door to the next room is unlocked
+                // The player can pick up different types of weapons- these will affect his damage to the monster
+                // The player must also manage their health bar, the monster can defend against attacks
             }
         }
     }
