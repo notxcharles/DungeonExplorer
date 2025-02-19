cd2550ce61d362157f02aee9ee0eee0fdc52f5b9
Charles <35784256+notxcharles@users.noreply.github.com>
2025-02-19 18:10:47 +0000

    Changed roomSize
    Changed how roomSize is calculated. Formally, the Room() class would +2 to each x and y dimension.
    Now, the roomSize includes the positions where the walls would be.
    roomSize now measures the outer perimeter rather than the inner perimeter

--- Game.cs
+++ Game.cs
@@ -17,11 +17,11 @@ namespace DungeonExplorer
         {
             // Change the playing logic into true and populate the while loop
             bool playing = false;
-            int[] roomSize = new int[2] { 5, 7 };
-            int[] doorCoord = new int[2] { 0, 5 };
+            int[] roomSize = new int[2] { 7, 9 }; //This should be viewed as room area, with +2 to each axis
+            int doorPosition = 5; // The door is always placed at the top of the room. Door Position controls the horizontal position
             int[] monsterCoord = new int[2] { 2, 3 };
             int[] treasureCoord = new int[2] { 5, 4 };
-            Room r = new Room("first", roomSize, doorCoord, monsterCoord, treasureCoord);
+            Room r = new Room("first", roomSize, doorPosition, monsterCoord, treasureCoord);
             while (playing)
             {
                 // Code your playing logic here
