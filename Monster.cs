64c77f74603189e15e4f56e77ed6bdfb8b4e9833
Charles <35784256+notxcharles@users.noreply.github.com>
2025-02-21 00:25:56 +0000

    Monster Class
    Monster class creates an object of a monster. Each monster has a name, a breed, health and does damage if attacked.
    GetAttackDamage() - the monster will do a random amount of damage, this is based on a normal distribution with the preset monster's attack damage as the mean.

---
+++ Monster.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer
+{
+    
+    class Monster
+    {
+        public string Name { get; private set; }
+        public string Breed { get; private set; }
+        public int Health { get; private set; }
+        private int m_averageAttack;
+        private Random m_random;
+        public Monster(string name, string breed, int health, int averageAttack)
+        {
+            Name = name;
+            Breed = breed;
+            Health = health;
+            m_averageAttack = averageAttack;
+            m_random = new Random();
+        }
+        private double CreateRandomGaussianNumber(int mean, int stdDev)
+        {
+            double u1 = 1.0 - m_random.NextDouble(); //uniform(0,1] random doubles
+            double u2 = 1.0 - m_random.NextDouble();
+            double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
+            double randNormal = mean + stdDev * randStdNormal; //random normal(mean,stdDev^2)
+            return randNormal;
+        }
+        public int GetAttackDamage()
+        {
+            //m_averageAttack represents the mean of a normal distribution
+            //attackDamage will be a random datapoint in the distribution
+            int stdDevPercentage = 5;
+            double multiplier = CreateRandomGaussianNumber(m_averageAttack, m_averageAttack / stdDevPercentage);
+            int attackDamage = Convert.ToInt32(m_averageAttack * multiplier);
+            return attackDamage;
+        }
+    }
+}
