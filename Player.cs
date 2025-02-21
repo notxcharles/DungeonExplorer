10038587c65db8b4963a37b150d6cebaecca6493
Charles <35784256+notxcharles@users.noreply.github.com>
2025-02-21 01:11:23 +0000

    Player GetDecision()
    GetDecision() prompts the player for their next action
    0- display inventory
    1- attack the monster in the room
    2- open the door
    9- exit the game
    function will return 0,1,2 or exit the game.

--- Player.cs
+++ Player.cs
@@ -24,8 +24,43 @@ namespace DungeonExplorer
         }
         public int GetDecision()
         {
-            throw new NotImplementedException();
-            return 0;
+            Console.WriteLine($"Your health: {this.Health}");
+            
+            bool recievedValidInput = false;
+            while (recievedValidInput == false)
+            {
+                Console.WriteLine("What do you want to do?");
+                Console.WriteLine("(0) View Inventory");
+                Console.WriteLine("(1) Attack Monster");
+                Console.WriteLine("(2) Open the door");
+                Console.WriteLine("(9) Exit game");
+                ConsoleKeyInfo key = Console.ReadKey();
+                try
+                {
+                    int keyAsInt = Convert.ToInt32(key.KeyChar.ToString());
+                    if (keyAsInt == 0 || keyAsInt == 1 || keyAsInt == 2)
+                    {
+                        Console.WriteLine($"Player pressed {keyAsInt}");
+                        return keyAsInt;
+                    }
+                    else if (keyAsInt == 9)
+                    {
+                        Console.WriteLine($"Player wishes to exit");
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{key} was pressed. You must press 0, 1, 2 or 9");
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine($"{key} was pressed. Please press 0, 1, 2 or 9");
+                }
+
+                
+            }
+            return -1;
         }
 
         public void ViewInventory()
