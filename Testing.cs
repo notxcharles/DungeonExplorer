aba79aa7062f59c736119b8429bcb692a6b3f180
notxcharles <35784256+notxcharles@users.noreply.github.com>
2025-03-12 19:57:10 +0000

    Remove un required code from testing class

--- Testing.cs
+++ Testing.cs
@@ -9,22 +9,6 @@ namespace DungeonExplorer
         {
 
         }
-        public void RunTests()
-        {
-            TestGetTurnDecisionsWithMonsterAlive();
-        }
-
-        private void TestGetTurnDecisionsWithMonsterAlive()
-        {
-            // Simulate valid user input within the range of 0 to 6
-            Player player = new Player("j", 100);
-            //Game game = new Game("gname", player);
-            bool monsterAlive = true;
-
-            // Use Debug.Assert to verify the expected outcome
-            int result = player.GetTurnDecisions(monsterAlive);
-            Debug.Assert(result >= 0 && result <= 6, $"Test failed: Expected a value between 0 and 6, but got {result}.");
-        }
         public static void TestForPositiveInteger(int value)
         {
             Debug.Assert(value > 0, "Error: Value wasn't a positive integer");
@@ -36,3 +20,4 @@ namespace DungeonExplorer
 
     }
 }
+ 
